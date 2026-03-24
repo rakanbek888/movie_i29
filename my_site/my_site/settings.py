6d3884278809e9a056e0846da69b5c20f7fc8722
rakanbek777 <_email@github.com>
2026-03-24 21:13:27 +0600

    Drf with Docker

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -136,8 +136,9 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
